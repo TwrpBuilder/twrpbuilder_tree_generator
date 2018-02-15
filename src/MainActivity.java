cf1dd525766f1adaa1da9f447eaf02fc395ab8e2
androidlover5842 <androidlover5842@gmail.com>
2018-02-15 21:58:15 +0530

    fix issue with spaces in filename

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,23 +1,42 @@
 
 import java.io.File;
 
+import util.ShellExecuter;
+
 public class MainActivity {
 	
 	private static int status;
+	private static File fname;
+	private static String rename;
+	private static String name;
 	public static void main(String[] a)
 	{
+		fname=new File(a[0]);
+		name=fname.toString();
+		if(name.contains(" "))
+		{
+			rename=name.replaceAll(" ","_");
+			fname=new File(rename);
+			ShellExecuter.commandnoapp("mv '"+name +"' "+rename);
+			name=rename;
+		}
+
+		
 		try {
 			System.out.println("Building tree from: "+a[0]);
-			if (new File(a[0]).exists())
+			if (fname.exists())
 			{
-			new Thread(new RunCode(a[0])).start();
+					ShellExecuter.commandnoapp("mv '"+name +"' "+rename);
+					new Thread(new RunCode(name)).start();
+
 			}else {
 				System.out.println("File not found");
+				System.exit(1);
 			}
 		}catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("invalid file name");
+			System.out.println("please enter file name");
 			System.exit(status);
 		}		
-		
+
 	}	
 }
