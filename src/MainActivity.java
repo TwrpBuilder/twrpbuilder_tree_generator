8c7c8fd452d4e7e8938f7e42f1375aaacabed65d
androidlover5842 <androidlover5842@gmail.com>
2018-02-16 20:58:48 +0530

    do not run if file has spaces

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -1,37 +1,27 @@
 
 import java.io.File;
-
 import util.RunCode;
-import util.ShellExecuter;
 
 public class MainActivity {
 	
 	private static int status;
 	private static File fname;
-	private static String rename;
 	private static String name;
 	public static void main(String[] a)
 	{
-		fname=new File(a[0]);
-		name=fname.toString();
-		if(name.contains(" "))
-		{
-			rename=name.replaceAll(" ","_");
-			fname=new File(rename);
-			if (!new File(rename).exists())
-			{
-			ShellExecuter.commandnoapp("mv '"+name +"' "+rename);
-			System.out.println("renaming file name to "+rename);
-			}
-			name=rename;
-		}
-		
-		
 		try {
-			System.out.println("Building tree from: "+name);
+			fname=new File(a[0]);
 			if (fname.exists())
 			{
+				if(!fname.getName().toString().contains(" "))
+				{
+					System.out.println("Building tree using: "+name);
 					new Thread(new RunCode(name)).start();
+				}else
+				{
+					System.out.println("remove spaces from file name");
+					System.exit(0);
+				}
 
 			}else {
 				System.out.println("File not found");
