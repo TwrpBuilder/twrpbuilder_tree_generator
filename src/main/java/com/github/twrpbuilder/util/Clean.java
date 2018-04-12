3ec40ce58dccc0a4c5639709a4914336fc9581c6
androidlover5842 <androidlover5842@gmail.com>
2018-04-12 23:47:12 +0530

    clean: add missing files list

--- src/main/java/com/github/twrpbuilder/util/Clean.java
+++ src/main/java/com/github/twrpbuilder/util/Clean.java
@@ -6,40 +6,23 @@ public class Clean {
 	private ShellExecutor shell;
 	public Clean(){
 		shell=new ShellExecutor();
-		shell.rm("build.prop" );
-		shell.rm("recovery.img");
-		if (file("mounts"))
-		{
-			shell.rm("mounts");
-		}
-		if (file("umkbootimg"))
-		{
-			shell.rm("umkbootimg");
-		}
-		
-		if (file(Config.outDir))
-		{
-			shell.rm(Config.outDir);
-		}
-		
-		if(file("unpack-MTK.pl"))
-		{
-			shell.rm("unpack-MTK.pl");
-		}
-		if (file("bin") || file("unpackimg.sh"))
-		{
-			shell.rm("unpackimg.sh");
-			shell.rm("bin");
-		}
-		if (file("magic"))
-		{
-			shell.rm("magic");
-		}
+		file("build.prop" );
+		file("recovery.img");
+		file("mounts");
+		file("umkbootimg");
+		file(Config.outDir);
+		file("unpack-MTK.pl");
+		file("unpackimg.sh");
+		file("bin");
+		file("magic");
+		file("androidbootimg.magic");
+
 	}
 
 	private boolean file(String name){
 		if (new File(name).exists())
 		{
+			shell.rm(name);
 			return true;
 		}
 		else {
